adb60a14160919ddce6c3c3cc4845a00a5e9154a
kevinkirby-1 <kjkirby96@gmail.com>
2024-02-01 16:54:29 -0700

    added access contol for front end

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
 
 app.use('/', require('./routes/contacts'));
 
